24b7bf62a5f87d60b9d4c9841e541aa65598e336
rajkumar2704 <rajkumarkurukanti@gmail.com>
2026-01-28 13:41:28 +0530

    added video ids to the code

--- video_stats.py
+++ video_stats.py
@@ -50,7 +50,47 @@ def get_video_ids(playlist_id):
                 break  
         return video_ids      
     except requests.exceptions.RequestException as e:
-        raise e    
+        raise e   
+
+def batch_list(video_id_list,batch_size):
+    for video_id in range(0, len(video_id_list), batch_size):
+        yield video_id_list[video_id:video_id + batch_size]   
+
+def extract_video_data(video_ids):
+    extracted_data = []
+
+    try:
+        for batch in batch_list(video_ids,maxResults):
+            videos_ids_str = ",".join(batch)
+
+            url = f"https://youtube.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&id={videos_ids_str}&key={API_KEY}"
+
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()  
+
+            for item in data.get('items',[]):
+                video_id = item['id']
+                snippet = item['snippet']
+                contentDetails = item['contentDetails']
+                statistics = item['statistics']
+
+                video_data = {
+                    "video_id": video_id,
+                    "title": snippet.get("title"),
+                    "publishedAt": snippet.get("publishedAt"),
+                    "duration": contentDetails.get("duration"),
+                    "viewCount": statistics.get("viewCount",None),
+                    "likeCount": statistics.get("likeCount",None),
+                    "commentCount": statistics.get("commentCount",None)
+                }   
+
+                extracted_data.append(video_data)
+        return extracted_data
+               
+    except requests.exceptions.RequestException as e:
+        raise e
 if __name__ == "__main__":
     playlistId = get_playlist_id()   
-    get_video_ids(playlistId)
+    video_ids = get_video_ids(playlistId)
+    extract_video_data(video_ids)
