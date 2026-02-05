7918d928b2df39e2a18812d6d07fb29147caa771
rajkumar2704 <rajkumarkurukanti@gmail.com>
2026-02-05 14:38:01 +0530

    added json logic

--- video_stats.py
+++ video_stats.py
@@ -2,6 +2,7 @@ import json
 import requests
 import os 
 from dotenv import load_dotenv
+from datetime import date 
 load_dotenv(dotenv_path="./.env")
 API_KEY = os.getenv("API_KEY")
 #API_KEY = "AIzaSyDcM3SSDL8g4db0qVrWOToGeNLFZI0GERI"
@@ -90,7 +91,15 @@ def extract_video_data(video_ids):
                
     except requests.exceptions.RequestException as e:
         raise e
+
+def save_to_json(extracted_data):
+    file_path = f"./data/YT_data_{date.today()}.json"
+
+    with open(file_path, "w", encoding="utf-8") as json_outfile:
+        json.dump(extracted_data, json_outfile, indent=4,ensure_ascii=False)
 if __name__ == "__main__":
     playlistId = get_playlist_id()   
     video_ids = get_video_ids(playlistId)
-    extract_video_data(video_ids)
+    video_data = extract_video_data(video_ids)
+    save_to_json(video_data)
+    
